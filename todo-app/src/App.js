9dd41c4163956cd0af4d5ad553aa8dc6941315f2
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-06-01 19:09:39 +0100

    uploading todo app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Firebase from 'firebase';
+import Header from './components/header/header.js';
+import Todo from './components/todo/todo.js';
+import Tachyons from 'tachyons';
+import db from './firebase.js'
+// import Form from './components/form/form.js';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const [input, setInput] = useState();
+
+  useEffect(() => {
+    db.collection('todos').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map((doc) => doc.data().title))
+    })
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection('todos').add({
+      title: input,
+    })
+    setInput("");
+  };
+
+  const deleteTodo = (e) => {
+    e.preventDefault();
+    let index = e.target.value;
+
+    todos.splice(index, 1);
+    setTodos([...todos]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App light-green">
+      <div className="card-container">
+        <div className="card shadow-5 ">
+          < Header />
+          <form>
+            <input className="br3 f6"
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className="button f5 green shadow-5 "
+              type="submit"
+              onClick={handleSubmit}
+              disabled={!input}
+            > Add</button>
+          </form>
+          {
+            todos.map((todo, index) => (
+              <Todo title={todo}
+                key={index}
+                value={index}
+                deleteTodo={deleteTodo} />
+            ))
+          }
+
+        </div>
+      </div>
     </div>
   );
 }
